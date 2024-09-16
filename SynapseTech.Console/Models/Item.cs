725645febaa3bf4906073d6a477699b285b2e8fd
Adu Asare Bediako <aluta182004@gmail.com>
2024-09-16 09:53:54 +0000

    added notes

--- SynapseTech.Console/Models/Item.cs
+++ SynapseTech.Console/Models/Item.cs
@@ -5,4 +5,7 @@ public class Item
     public string? ItemId { get; set; }
     public string? Name { get; set; }
     public string? Status { get; set; }
+    public string? Description { get; set; }
+
+    public string? deliveryNotification { get; set; }
 }
